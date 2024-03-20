1d02ad39665d7df80683c97d18a06eb5059fa64d
aesthetix-store <aesthetixplusstore@gmail.com>
2024-03-20 17:41:16 +0600

    custom attribute optional

--- src/models/products.ts
+++ src/models/products.ts
@@ -2,10 +2,11 @@ import { Column, Entity } from "typeorm"
 import {
   // alias the core entity to not cause a naming conflict
   Product as MedusaProduct,
+  nullableValue,
 } from "@medusajs/medusa"
 
 @Entity()
 export class Product extends MedusaProduct {
-  @Column()
-  customAttribute: string
+  @Column({nullable:true})
+  customAttribute?: string
 }
