1ecffe7e2d26ac89fdd4eda5b5098145154bbfef
aesthetix-store <aesthetixplusstore@gmail.com>
2024-03-22 20:51:39 +0600

    custom_field is optional

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,12 @@ import { registerOverriddenValidators } from "@medusajs/medusa"
 import {
    AdminPostProductsReq as MedusaAdminPostProductsReq,
 } from "@medusajs/medusa/dist/api/routes/admin/products/create-product"
-import { IsString } from "class-validator"
+import { IsString, IsOptional } from "class-validator"
 
 class AdminPostProductsReq extends MedusaAdminPostProductsReq {
    @IsString()
-   custom_field: string
+   @IsOptional()
+   custom_field: string 
 }
 
 registerOverriddenValidators(AdminPostProductsReq)
